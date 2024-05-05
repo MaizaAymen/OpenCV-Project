38d7887ade20c4d3e097f233c2f1fc4704f12ad9
unknown <maizaaymen>
2024-05-05 13:16:30 +0100

    OpenCV-Project

---
+++ aymen.py
@@ -0,0 +1,66 @@
+# def Dliste(liste):
+#     if not liste:
+#           return None
+#    else:
+
+#       return liste[-1]
+
+# list=["A","R","b","n"]
+# print(Dliste(list))
+# def Dliste(liste):
+#     return"True" if liste[-1]=='s' else "False"
+
+
+# A=input("donner une chaine : ")
+# print(Dliste(A) )
+
+# def Nombre(liste):
+# return "paire" if liste%2==0 else "impaire"
+
+
+# A=int(input("donner une nombre : "))
+# print(Nombre(A))
+
+# def Nombre(liste):
+# return "pair" if liste%2==0 else "impair"
+
+# print(Nombre(9) )
+# def TRUE(liste):
+# return sum(1 for i in liste if i=="true")
+# print(TRUE(["true","false","false","true","false"]))
+# aymen="bonjour|je|suis|aymen|maiza"
+# print(aymen.split("|"))
+
+# liste=["1","2","3","4","5"]
+# n=len(liste)
+# i=0
+# for i in range(n):
+# print(liste[i])
+
+# liste=["python","java","C++","javascript"]
+# n=len(liste)
+# i=0
+# A=liste[0]
+# B=liste[-1]
+
+# for i in range(n):
+# liste[-1]=A
+# liste[0]=B
+
+# print(liste)
+
+#liste = ["python", "aymen", "slm", "java", "C++", "javascript"]
+#liste1 = ""
+#liste2 = ""
+#liste3=""
+#i=0
+#n = len(liste)
+#for i in range(n):
+ #   if i%2==0:
+  #      liste1 += liste[i]
+   # else:
+    #    liste2 += liste[i]
+
+
+#liste3="[" + liste1 +"]" +"[" + liste2 + "]"
+#print(liste3)
